aef5c84f471baca21ce4ee86ea6f716a4d444190
rohanibra <rohanroman52@gmail.com>
2024-02-18 20:01:24 +0600

    half worked done

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,24 @@
-function play(){
-    console.log('play start now');
+// function play(){
+// //    step 1: hide the home screen to hide the screen add the class to the home section.
+// const homeSection=document.getElementById('home-screen');
+// homeSection.classList.add('hidden')
+// // play ground
+// const playGroundSection=document.getElementById('play-ground');
+// playGroundSection.classList.remove('hidden');
+// }
+function continueGame() {
+    // step-1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    // set random generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+    setBackgroundColorById(alphabet);
+}
+
+
+
+function play() {
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
 }
