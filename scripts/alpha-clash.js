cd1c97f9cb2893df977b03b3ed08b8422949dec5
rohanibra <rohanroman52@gmail.com>
2024-02-19 13:13:39 +0600

    finally almost done the project

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -11,6 +11,11 @@ function handleKeyBoardPress(e) {
     const playerPressed = e.key;
     console.log('player pressed:', playerPressed);
 
+    // stop the game 
+    if(playerPressed==='Escape'){
+        gameOver();
+    }
+
     // get the expected to press
     const currentAlphabetElement = document.getElementById('current-alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
@@ -20,18 +25,23 @@ function handleKeyBoardPress(e) {
     // check match or not
     if (playerPressed === expectedAlphabet) {
         console.log('you get a point');
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+
+        // --------------------------------------
         // update score
         // 1. get the current score
-        const currentScoreElement= document.getElementById('current-score');
-        const currentScoreText =currentScoreElement.innerText;
-        const currentScore=parseInt(currentScoreText);
-        console.log(currentScore);
+        // const currentScoreElement= document.getElementById('current-score');
+        // const currentScoreText =currentScoreElement.innerText;
+        // const currentScore=parseInt(currentScoreText);
+        // console.log(currentScore);
 
-        // increase the score by 1
-        const newScore= currentScore + 1;
-        // show the update score
-        currentScoreElement.innerText = newScore;
-        // start a new round
+        // // increase the score by 1
+        // const newScore= currentScore + 1;
+        // // show the update score
+        // currentScoreElement.innerText = newScore;
+        // // start a new round
 
 
 
@@ -40,15 +50,30 @@ function handleKeyBoardPress(e) {
     }
     else {
         console.log('you missed. and you lost a life');
-        // step-1: get the current life number
-        const currentLifeElement =document.getElementById('current-life');
-        const currentLifeText =currentLifeElement.innerText;
-        const lifeCount = parseInt(currentLifeText);
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
 
-        //step-2: reduce the life count
-        const newLife = lifeCount-1;
-        // step-3: update the life score
-        currentLifeElement.innerText =newLife;
+
+
+
+
+
+
+
+        // -------------------------------------------------
+        // // step-1: get the current life number
+        // const currentLifeElement =document.getElementById('current-life');
+        // const currentLifeText =currentLifeElement.innerText;
+        // const lifeCount = parseInt(currentLifeText);
+
+        // //step-2: reduce the life count
+        // const newLife = lifeCount-1;
+        // // step-3: update the life score
+        // currentLifeElement.innerText =newLife;
+        if (updatedLife === 0) {
+            gameOver();
+        }
     }
 }
 // capture keyboard key press
@@ -70,7 +95,31 @@ function continueGame() {
 
 
 function play() {
+    // hide everything show the playground
     hideElementById('home-screen');
+    hideElementById('final-score')
     showElementById('play-ground');
+
+    // reset score and life
+    setTextElementValueById(
+    'current-life', 5);
+    setTextElementValueById('current-score', 0)
+
     continueGame();
+}
+
+function gameOver() {
+    hideElementById('play-ground');
+    showElementById('final-score');
+    // update final score
+    // 1. get the final score
+    const lastScore =getTextElementValueById('current-score');
+    console.log(lastScore);
+    setTextElementValueById('last-score' , lastScore);
+
+    // clear the last selected alphabet
+    const currentAlphabet = getElementTextById('current-alphabet');
+    // console.log(currentAlphabet);
+    removeBackgroundColorById(currentAlphabet);
+
 }
