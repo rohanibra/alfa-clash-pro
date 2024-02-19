77623203998029e7b8ea4852c2ce8a6a3e6a6221
rohanibra <rohanroman52@gmail.com>
2024-02-19 11:57:19 +0600

    current life score added

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -14,15 +14,41 @@ function handleKeyBoardPress(e) {
     // get the expected to press
     const currentAlphabetElement = document.getElementById('current-alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
-    const expectedAlphabet=currentAlphabet.toLocaleLowerCase();
-    console.log(playerPressed ,expectedAlphabet);
+    const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+    console.log(playerPressed, expectedAlphabet);
 
     // check match or not
-    if( playerPressed === expectedAlphabet){
+    if (playerPressed === expectedAlphabet) {
         console.log('you get a point');
+        // update score
+        // 1. get the current score
+        const currentScoreElement= document.getElementById('current-score');
+        const currentScoreText =currentScoreElement.innerText;
+        const currentScore=parseInt(currentScoreText);
+        console.log(currentScore);
+
+        // increase the score by 1
+        const newScore= currentScore + 1;
+        // show the update score
+        currentScoreElement.innerText = newScore;
+        // start a new round
+
+
+
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
     }
-    else{
+    else {
         console.log('you missed. and you lost a life');
+        // step-1: get the current life number
+        const currentLifeElement =document.getElementById('current-life');
+        const currentLifeText =currentLifeElement.innerText;
+        const lifeCount = parseInt(currentLifeText);
+
+        //step-2: reduce the life count
+        const newLife = lifeCount-1;
+        // step-3: update the life score
+        currentLifeElement.innerText =newLife;
     }
 }
 // capture keyboard key press
@@ -38,7 +64,7 @@ function continueGame() {
     const currentAlphabetElement = document.getElementById('current-alphabet');
     currentAlphabetElement.innerText = alphabet;
     setBackgroundColorById(alphabet);
-    console.log('random alphabet:', alphabet);
+    // console.log('random alphabet:', alphabet);
 }
 
 
